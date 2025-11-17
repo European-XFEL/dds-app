2d2117f40b636b22804cac7b15eba0306333353d
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-11-17 07:35:01 +0100

    feat: add dir for custom components, add shell/sidebar

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 	kit: {
     adapter: adapter(),
     alias: {
-      "@/*": "./path/to/lib/*",
+      "$components": "./src/components",
     },
   },
 	extensions: ['.svelte', '.svx']
