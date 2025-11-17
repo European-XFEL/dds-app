05d76f103f627a98c266f992f61b5e511d7c362e
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-11-17 07:46:09 +0100

    chore: remove unused breadcrumbs, remove unused sidebar route

--- src/components/layout/shell.svelte
+++ src/components/layout/shell.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-	import AppSidebar from '$components/layout/sidebar.svelte';
-	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
-	import { Separator } from '$lib/components/ui/separator/index.js';
-	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import '../../app.css';
+	import '$css';
 	import favicon from '$lib/assets/favicon.svg';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import AppSidebar from '$components/layout/sidebar.svelte';
 
 	let { children } = $props();
 </script>
@@ -16,25 +14,8 @@
 <Sidebar.Provider>
 	<AppSidebar />
 	<Sidebar.Inset>
-		<header class="flex h-16 shrink-0 items-center gap-2">
-			<div class="flex items-center gap-2 px-4">
-				<Sidebar.Trigger class="-ml-1" />
-				<Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
-				<Breadcrumb.Root>
-					<Breadcrumb.List>
-						<Breadcrumb.Item class="hidden md:block">
-							<Breadcrumb.Link href="##">Dashboard</Breadcrumb.Link>
-						</Breadcrumb.Item>
-						<Breadcrumb.Separator class="hidden md:block" />
-						<Breadcrumb.Item>
-							<Breadcrumb.Page>Setup</Breadcrumb.Page>
-						</Breadcrumb.Item>
-					</Breadcrumb.List>
-				</Breadcrumb.Root>
-			</div>
-		</header>
 		<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
-		{@render children()}
+			{@render children()}
 		</div>
 	</Sidebar.Inset>
 </Sidebar.Provider>
