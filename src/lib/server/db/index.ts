156b11ed4f6119a4872c447fe9a2de0f4a78f098
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-11-26 11:05:37 +0100

    fix(db): use drizzle directly for building client, import node implementation

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,17 +3,13 @@ import fs from 'fs/promises';
 
 import { env } from '$env/dynamic/private';
 
-import { drizzle } from 'drizzle-orm/libsql';
-import { createClient } from '@libsql/client';
-
+import { drizzle } from 'drizzle-orm/libsql/node';
 import * as schema from './schema';
 import type { SQLiteTableWithColumns } from 'drizzle-orm/sqlite-core/table';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = createClient({ url: env.DATABASE_URL });
-
-export const db = drizzle(client, { schema });
+export const db = drizzle(env.DATABASE_URL, { schema });
 
 /**
  * Bootstrap function that inserts example data into the table.
