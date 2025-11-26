6ec011cbe1bea642c3ff86061a635ad6b440586f
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-11-26 11:01:58 +0100

    feat(components/detector): add detector card component

---
+++ src/components/detector/detector-card.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import DetectorCard from '$components/ui/detector/detector-card.svelte';
+	import { appState } from '$lib/state.svelte';
+
+	let { q_vals, detector } = $state(appState);
+
+	let { short = false } = $props();
+</script>
+
+<DetectorCard bind:detector bind:q_vals {short} />
