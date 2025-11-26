7d713f739f3f8d7bef0e54bf045cb1b3d76bc622
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-11-26 04:41:22 +0100

    feat(components/sample): add reusable (instantiated) components

---
+++ src/components/sample-card-ground.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/state';
+
+	import { MoleculeCard } from '$components/sample';
+	import { appState } from '$lib/state.svelte';
+
+	let { sample } = $state(appState);
+</script>
+
+<MoleculeCard
+	title="Ground Molecule"
+	molecules={page.data.molecules}
+	bind:molecule={sample.groundMolecule}
+/>
