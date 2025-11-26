015a7cd1194ed2e27540ce537d7dc24775c1663a
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-11-26 11:04:21 +0100

    feat(types): tidy field names, add refinement flags

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,30 +1,43 @@
 import * as schema from '$lib/server/db/schema';
 
 export type SampleDetails = {
-	groundMolecule: typeof schema.moleculeTable.$inferSelect;
-	excitedMolecule: typeof schema.moleculeTable.$inferSelect;
+	ground: typeof schema.moleculeTable.$inferSelect;
+	excited: typeof schema.moleculeTable.$inferSelect;
 	solvent: typeof schema.solventTable.$inferSelect;
 	concentration: number;
 };
 
-export type PPDetails = {
-	laserEnergy?: number;
-	excitedFrac?: number;
-	excitedPotential?: number;
+export type Pump = {
+	energy: number;
+	excitedFrac: number;
+	excitedPotential: number;
 };
 
 export type QVals = {
-	min?: number;
-	max?: number;
-	step?: number;
+	min: number;
+	max: number;
+	step: number;
 };
 
 export type Detector = {
-	name?: string;
+	name: string;
+	pixel_size: number;
+	distance: number;
+	wavelength: number;
+	shape: [number, number];
+	beam_center: [number, number];
+};
+
+export type RefinementFlags = {
+	apply_geometry_mask: boolean;
+	apply_bad_pixel_mask: boolean;
+	apply_noise: boolean;
 };
 
 export type SimulationDetails = {
 	sample: SampleDetails;
-	qVals: QVals;
+	q_vals: QVals;
 	detector: Detector;
+	pump: Pump;
+	refinement_flags?: RefinementFlags;
 };
