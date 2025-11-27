225c2f9d31567eb5e0c871a3abb109fa60c79f15
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-11-27 08:47:16 +0100

    feat(db): add function to dump db for static builds

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,13 +1,15 @@
-import { db } from '$lib/server/db';
+import * as db from '$lib/server/db';
 import * as schema from '$lib/server/db/schema';
 
 import type { LayoutServerLoad } from './$types';
 
 export const prerender = true;
 
+export const _db = await db.setup_db();
+
 export const load: LayoutServerLoad = async ({}) => {
   return {
-    molecules: await db.select().from(schema.moleculeTable),
-    solvents: await db.select().from(schema.solventTable),
+    molecules: await _db.select().from(schema.moleculeTable),
+    solvents: await _db.select().from(schema.solventTable),
   };
 };
