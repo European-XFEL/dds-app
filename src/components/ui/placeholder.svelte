cc95e915fb2847c25c52948de0f0c228e3c912c9
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-11-28 12:39:07 +0100

    feat(components): add and use stripey labeled placeholder

---
+++ src/components/ui/placeholder.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { HTMLAttributes } from 'svelte/elements';
+
+  let {
+    label,
+    ...restProps
+  }: {
+    label: string;
+  } & HTMLAttributes<HTMLDivElement> = $props();
+</script>
+
+<div {...restProps} class={['relative', restProps.class]}>
+  <div
+    class="h-full w-full bg-[repeating-linear-gradient(45deg,var(--muted),var(--muted)_8px,var(--border)_2px,var(--border)_10px)]"
+  ></div>
+  <div class="absolute inset-0 flex items-center justify-center">
+    <p>{label}</p>
+  </div>
+</div>
