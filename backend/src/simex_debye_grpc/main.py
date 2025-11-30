0181778f3999959e535424a0af3cffd2e4b9fc36
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-11-30 13:20:41 +0100

    feat(backend): use graceful exit, add debug response print

--- backend/src/simex_debye_grpc/main.py
+++ backend/src/simex_debye_grpc/main.py
@@ -1,4 +1,5 @@
 from grpclib.server import Server
+from grpclib.utils import graceful_exit
 
 from . import models
 from .calculator import SimulationCalculator
@@ -10,18 +11,28 @@ class SimulationService(models.SimulationServiceBase):
     ) -> models.SimulationResponse:
         calculator = SimulationCalculator(message)
         response = calculator.run()
+        print(
+            f"SimulationResponse: q={len(response.q)}, "
+            f"delta_s={len(response.delta_s)}, "
+            f"delta_s_solute_ex_frac={len(response.delta_s_solute_ex_frac)}, "
+            f"delta_s_solvent={len(response.delta_s_solvent)}, "
+            f"deposited_energy_joule={response.deposited_energy_joule}, "
+            f"delta_temperature_k={response.delta_temperature_k}, "
+            f"solvent_to_solute_ratio={response.solvent_to_solute_ratio}"
+        )
         return response
 
 
-async def start_server():
-    HOST = "localhost"
-    PORT = 50051
+async def main(*, host="127.0.0.1", port=50051):
     server = Server([SimulationService()])
-    await server.start(HOST, PORT)
-    await server.wait_closed()
+
+    with graceful_exit([server]):
+        await server.start(host, port)
+        print(f"Serving on {host}:{port}")
+        await server.wait_closed()
 
 
 if __name__ == "__main__":
     import asyncio
 
-    asyncio.run(start_server())
+    asyncio.run(main())
