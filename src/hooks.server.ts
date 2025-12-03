4800071559c64e1dfec6092993156138a84bb813
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-12-03 08:25:32 +0100

    feat(db): move seed function, add seed command

--- src/hooks.server.ts
+++
@@ -1,7 +0,0 @@
-import type { ServerInit } from '@sveltejs/kit';
-
-import { bootstrap } from '$lib/server/db';
-
-export const init: ServerInit = async () => {
-  await bootstrap();
-};
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,7 +1,4 @@
-import { drizzle } from 'drizzle-orm/libsql/node';
-import type { SQLiteTableWithColumns } from 'drizzle-orm/sqlite-core/table';
-import fs from 'fs/promises';
-import path from 'path';
+import { drizzle } from 'drizzle-orm/pglite';
 
 import { env } from '$env/dynamic/private';
 
@@ -11,58 +8,10 @@ export async function setup_db() {
   if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
   const db = drizzle(env.DATABASE_URL, { schema });
+
   return db;
 }
 
-/**
- * Bootstrap function that inserts example data into the table.
- *
- * The IDs for example data are hardcoded to `example-{filename}` so that this function can be
- * re-run without creating duplicate entries, instead it will just overwrite existing example entries.
- */
-export async function bootstrap(
-  db: ReturnType<typeof drizzle> | null = null,
-  example_molecules_dir = './static/data/molecules',
-  example_solvents_dir = './static/data/solvents',
-) {
-  if (!db) {
-    db = await setup_db();
-  }
+export const db = await setup_db();
 
-  // Define schema directory pairs
-  let schema_file_pairs: [SQLiteTableWithColumns<any>, string, string[]][] = [
-    [schema.moleculeTable, example_molecules_dir, await fs.readdir(example_molecules_dir)],
-    [
-      schema.solventTable,
-      example_solvents_dir,
-      await fs
-        .readdir(example_solvents_dir)
-        .then((files) => files.filter((f) => !f.endsWith('-error.txt'))),
-    ],
-  ];
-
-  for (const [table, root, files] of schema_file_pairs) {
-    for (const file of files) {
-      console.log(` - ${file}`);
-      // Strip file extension for name
-      let name = file.replace(/\.[^/.]+$/, '');
-      const contents = await fs.readFile(path.join(root, file), 'utf-8');
-      await db
-        .insert(table)
-        .values({
-          id: `${file}`,
-          filename: file,
-          name: name,
-          contents: contents,
-        })
-        .onConflictDoUpdate({
-          target: table.id,
-          set: {
-            name: name,
-            filename: file,
-            contents: contents,
-          },
-        });
-    }
-  }
-}
+export { schema };
