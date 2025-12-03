8db1d210ba836c14c845cb2607421e257d756ac3
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-12-03 21:46:02 +0100

    feat(components/sample): async load data

--- src/lib/sample/components/molecule-card.svelte
+++ src/lib/sample/components/molecule-card.svelte
@@ -1,36 +1,57 @@
 <script lang="ts">
+  import { getMoleculeFileContent } from '../../../routes/data.remote';
   import { Upload } from '@lucide/svelte';
   import { Structure } from 'matterviz';
+
+  import { onMount } from 'svelte';
+  import { fade } from 'svelte/transition';
 
   import { Button } from '$shadcn/ui/button/index.js';
   import * as Card from '$shadcn/ui/card/index.js';
   import * as Field from '$shadcn/ui/field/index.js';
   import * as Select from '$shadcn/ui/select/index.js';
+  import { Spinner } from '$shadcn/ui/spinner';
 
   import type { Sample } from '$lib/types';
-
   import LabeledPlaceholder from '$lib/ui/components/placeholder.svelte';
 
-  let {
+  type Molecules = { id: string; name: string }[];
+
+  const {
     title,
-    molecules,
+    molecules: _molecules,
     molecule = $bindable(),
   }: {
     title: string;
-    molecules: { id: string; filename: string; name: string; content: string }[];
+    molecules: Molecules | Promise<Molecules>;
     molecule: Sample['ground'] | Sample['excited'];
   } = $props();
 
-  const triggerMolecule = $derived(
-    molecules.find((m) => m.id === molecule.id)?.name ?? 'Select a molecule',
+  let molecules = $state<Molecules>([]);
+  let loading = $state(true);
+
+  onMount(() => {
+    Promise.resolve(_molecules).then((data) => {
+      molecules = data;
+      loading = false;
+    });
+  });
+
+  let molecule_file_contents = $derived(
+    getMoleculeFileContent(molecule.id).then((data) => data?.fileTable.contents),
   );
 
-  const data_url = $derived(molecule?.id ? '/data/molecules/' + molecule.id : undefined);
+  const triggerMolecule = $derived(
+    molecules.find((m) => m.id === molecule.id)?.name ?? 'Select molecule',
+  );
 </script>
 
 <Card.Root class="flex-auto @sm:gap-3">
   <Card.Header>
-    <Card.Title>{title}</Card.Title>
+    <Card.Title
+      >{title}
+      {#if loading}<div class="absolute ml-2 inline-block"><Spinner /></div>{/if}
+    </Card.Title>
   </Card.Header>
 
   <Card.Content>
@@ -40,10 +61,12 @@
           <Select.Root
             name="molecule"
             type="single"
+            disabled={loading}
             bind:value={
               () => molecule.id,
               (v) => {
-                molecule = molecules.find((m) => m.id === v) ?? molecule;
+                molecule.id = v;
+                molecule.name = triggerMolecule;
               }
             }
           >
@@ -66,10 +89,23 @@
       <Field.Field>
         <Field.Content>
           <div class="h-120 w-full min-w-150 border border-muted/50">
-            {#if !molecule?.id}
-              <LabeledPlaceholder label="No molecule selected" class="h-full w-full opacity-50" />
+            {#if molecule.id === ''}
+              <span transition:fade>
+                <LabeledPlaceholder label="No molecule selected" class="h-full w-full opacity-50" />
+              </span>
             {:else}
-              <Structure {data_url} style="height: 100%; width: 100%" />
+              {#await molecule_file_contents}
+                <span transition:fade>
+                  <LabeledPlaceholder
+                    label="No molecule selected"
+                    class="h-full w-full opacity-50"
+                  />
+                </span>
+              {:then structure_string}
+                <span transition:fade={{ delay: 200 }}>
+                  <Structure {structure_string} style="height: 100%; width: 100%" />
+                </span>
+              {/await}
             {/if}
           </div>
         </Field.Content>
