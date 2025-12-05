3576361bcc4e14b22a1673fe89f641fa64481d85
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-12-05 16:28:29 +0100

    build(fix): narrow imports for better builds

--- src/lib/plots/line-chart.svelte
+++ src/lib/plots/line-chart.svelte
@@ -1,11 +1,46 @@
 <script lang="ts">
-  import * as echarts from 'echarts';
-  import type { EChartsOption } from 'echarts';
+  // Tree-shakeable ECharts imports - only include what you use
+  import * as echarts from 'echarts/core';
+  import { LineChart, type LineSeriesOption } from 'echarts/charts';
+  import {
+    TitleComponent,
+    type TitleComponentOption,
+    TooltipComponent,
+    type TooltipComponentOption,
+    GridComponent,
+    type GridComponentOption,
+    LegendComponent,
+    type LegendComponentOption,
+    DataZoomComponent,
+    type DataZoomComponentOption,
+  } from 'echarts/components';
+  import { CanvasRenderer } from 'echarts/renderers';
+
+  // Register only the components you need
+  echarts.use([
+    LineChart,
+    TitleComponent,
+    TooltipComponent,
+    GridComponent,
+    LegendComponent,
+    DataZoomComponent,
+    CanvasRenderer,
+  ]);
+
+  // Compose type for type-safe options
+  type ECOption = echarts.ComposeOption<
+    | LineSeriesOption
+    | TitleComponentOption
+    | TooltipComponentOption
+    | GridComponentOption
+    | LegendComponentOption
+    | DataZoomComponentOption
+  >;
 
   type Props = {
-    constant_options: Omit<EChartsOption, 'series' | 'xAxis'>;
-    series: EChartsOption['series'];
-    xAxis: EChartsOption['xAxis'];
+    constant_options: Omit<ECOption, 'series' | 'xAxis'>;
+    series: ECOption['series'];
+    xAxis: ECOption['xAxis'];
   };
 
   let { constant_options, series, xAxis }: Props = $props();
