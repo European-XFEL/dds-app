c776f02e601a9953474ed40f3947512e73d6bc3f
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-12-08 11:11:27 +0100

    feat(grpc/gen): update grpc client

--- src/lib/server/grpc/index.ts
+++ src/lib/server/grpc/index.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@connectrpc/connect';
-import { createGrpcTransport } from '@connectrpc/connect-node';
+import { createConnectTransport } from '@connectrpc/connect-node';
 
 import { FileService } from './gen/files_pb';
 import { SimulationService } from './gen/simulation_pb';
 
 // Export the types for internal use
-export { SimulationService, FileService, createClient, createGrpcTransport };
+export { SimulationService, FileService, createClient, createConnectTransport };
