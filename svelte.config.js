b32b03f894befa2a522e15044d5f93b513441d67
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-12-14 13:08:53 +0100

    build: revert back to static adapter, add svelteflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,11 @@
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from 'mdsvex';
-
-import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
-  preprocess: [vitePreprocess(), mdsvex()],
+  preprocess: [vitePreprocess()],
   kit: {
     adapter: adapter(),
     alias: {
