d701d65a7813ebf3f0c63bd77ec0ab6a9c0d249f
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-12-16 14:25:57 +0100

    feat(storybook/probe): add probe card story

---
+++ src/lib/probe/ProbeSetupCard.stories.svelte
@@ -0,0 +1,21 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+
+  import { createSimulationSeed } from '$lib/state.svelte';
+
+  import ProbeSetupCard from './ProbeSetupCard.svelte';
+
+  const defaultState = createSimulationSeed();
+
+  const { Story } = defineMeta({
+    component: ProbeSetupCard,
+    args: {
+      probe: defaultState.probe,
+      short: false,
+    },
+  });
+</script>
+
+<Story name="Default" />
+
+<Story name="Short" args={{ short: true }} />
