f1a3877307c72a69692ec2ebf8c92868bd310d42
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-12-16 13:47:04 +0100

    feat(storybook/detector): add detector stories

---
+++ src/lib/detector/DetectorCakeCard.stories.svelte
@@ -0,0 +1,23 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+
+  import { createSimulationSeed } from '$lib/state.svelte';
+
+  import DetectorCakeCard from './DetectorCakeCard.svelte';
+
+  const defaultState = createSimulationSeed();
+
+  const { Story } = defineMeta({
+    component: DetectorCakeCard,
+    args: {
+      beamCenter: defaultState.detector.beamCenter,
+      imageShape: defaultState.detector.imageShape,
+      modules: defaultState.detector.modules,
+    },
+    parameters: {
+      layout: 'fullscreen',
+    },
+  });
+</script>
+
+<Story name="Default" />
---
+++ src/lib/detector/DetectorSetupCard.stories.svelte
@@ -0,0 +1,19 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+
+  import { createSimulationSeed } from '$lib/state.svelte';
+
+  import DetectorSetupCard from './DetectorSetupCard.svelte';
+
+  const defaultState = createSimulationSeed();
+
+  const { Story } = defineMeta({
+    component: DetectorSetupCard,
+    args: {
+      distance: defaultState.detector.distance,
+      beamCenter: defaultState.detector.beamCenter,
+    },
+  });
+</script>
+
+<Story name="Default" />
