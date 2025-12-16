260c831d78096d4965000b1dd9c9b15504137975
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-12-16 14:15:30 +0100

    style(detector): minor style updates

--- src/lib/detector/DetectorInfoCard.svelte
+++ src/lib/detector/DetectorInfoCard.svelte
@@ -10,7 +10,7 @@
   let { ...restProps }: Props = $props();
 </script>
 
-<Card.Root class="flex-1 min-w-sm">
+<Card.Root class="min-w-sm flex-1">
   <Card.Content>
     <DetectorInfo {...restProps} />
   </Card.Content>
