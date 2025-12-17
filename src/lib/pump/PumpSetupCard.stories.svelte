f24be597fed6ac4536638b294687851e56a495f8
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-12-17 09:26:08 +0100

    feat(storybook/pump): add story for PumpSetupCard

---
+++ src/lib/pump/PumpSetupCard.stories.svelte
@@ -0,0 +1,18 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+
+  import { createSimulationSeed } from '$lib/state.svelte';
+
+  import PumpSetupCard from './PumpSetupCard.svelte';
+
+  const defaultState = createSimulationSeed();
+
+  const { Story } = defineMeta({
+    component: PumpSetupCard,
+    args: {
+      pump: defaultState.pump,
+    },
+  });
+</script>
+
+<Story name="Default" />
