34f1aec46b861b8a72de4225cbb06ca849356fcb
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-12-17 09:26:55 +0100

    feat(storybook/sample): add story for MoleculeCard, SolventCard, SolventInfoCard

---
+++ src/lib/sample/SolventCard.stories.svelte
@@ -0,0 +1,23 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+
+  import { createSimulationSeed } from '$lib/state.svelte';
+
+  import SolventCard from './SolventCard.svelte';
+
+  const defaultState = createSimulationSeed();
+
+  const { Story } = defineMeta({
+    component: SolventCard,
+    args: {
+      solvent: defaultState.sample.solvent,
+      concentrationSoluteMolar: defaultState.sample.concentrationSoluteMolar,
+      qRange: defaultState.qRange,
+      short: false,
+    },
+  });
+</script>
+
+<Story name="Default" />
+
+<Story name="Short" args={{ short: true }} />
