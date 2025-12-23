642408a056789dfa11522d72cc6e567fc25d73e7
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-12-23 14:21:25 +0100

    refactor(detector): simplify form component use

--- src/lib/detector/components/ui/DetectorSetup.svelte
+++ src/lib/detector/components/ui/DetectorSetup.svelte
@@ -23,51 +23,45 @@
   let detectorName: string | undefined = $state('LPD');
 </script>
 
-<div>
-  <form>
-    <Field.Set>
-      <Field.Legend>Detector Configuration</Field.Legend>
-      <Field.Description>Select detector type and configure module positions.</Field.Description>
+<Field.Set>
+  <Field.Legend>Detector Configuration</Field.Legend>
+  <Field.Description>Select detector type and configure module positions.</Field.Description>
 
-      <Field.Field>
-        <Field.Label>Detector</Field.Label>
-        <Select.Root type="single" bind:value={detectorName}>
-          <Select.Trigger>
-            <span>
-              {detectorName || 'Select Detector'}
-            </span>
-          </Select.Trigger>
-          <Select.Content>
-            {#each detectors as { name, enabled } (name)}
-              <Select.Item value={name} label={name} disabled={!enabled}>
-                {name}
-              </Select.Item>
-            {/each}
-          </Select.Content>
-        </Select.Root>
-      </Field.Field>
+  <Field.Field>
+    <Field.Label>Detector</Field.Label>
+    <Select.Root type="single" bind:value={detectorName}>
+      <Select.Trigger>
+        <span>{detectorName || 'Select Detector'}</span>
+      </Select.Trigger>
+      <Select.Content>
+        {#each detectors as { name, enabled } (name)}
+          <Select.Item value={name} label={name} disabled={!enabled}>
+            {name}
+          </Select.Item>
+        {/each}
+      </Select.Content>
+    </Select.Root>
+  </Field.Field>
 
-      <Field.Field>
-        <Field.Label>Sample Detector Distance (mm)</Field.Label>
-        <div class="flex min-w-fit items-center justify-between gap-x-2">
-          <Input
-            type="number"
-            bind:value={distance}
-            class="nodrag w-30 text-sm text-muted-foreground"
-          />
-          <Input type="range" bind:value={distance} class="nodrag" />
-        </div>
-      </Field.Field>
+  <Field.Field>
+    <Field.Label>Sample Detector Distance (mm)</Field.Label>
+    <div class="flex min-w-fit items-center justify-between gap-x-2">
+      <Input
+        type="number"
+        bind:value={distance}
+        class="nodrag w-30 text-sm text-muted-foreground"
+      />
+      <Input type="range" bind:value={distance} class="nodrag" />
+    </div>
+  </Field.Field>
 
-      <Field.Group class="flex flex-wrap">
-        <Field.Field>
-          <Field.Label>Beam Center (px)</Field.Label>
-          <div class="grid grid-cols-2 gap-4">
-            <Input type="number" bind:value={beamCenter.x} />
-            <Input type="number" bind:value={beamCenter.y} />
-          </div>
-        </Field.Field>
-      </Field.Group>
-    </Field.Set>
-  </form>
-</div>
+  <Field.Group class="flex flex-wrap">
+    <Field.Field>
+      <Field.Label>Beam Center (px)</Field.Label>
+      <div class="grid grid-cols-2 gap-4">
+        <Input type="number" bind:value={beamCenter.x} />
+        <Input type="number" bind:value={beamCenter.y} />
+      </div>
+    </Field.Field>
+  </Field.Group>
+</Field.Set>
