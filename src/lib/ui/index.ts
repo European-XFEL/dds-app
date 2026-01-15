69bb5771b53e34d0b8b712f47d91cb07b8ad30fb
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2026-01-15 13:19:22 +0100

    feat(dashboard): add setup checklist

--- src/lib/ui/index.ts
+++ src/lib/ui/index.ts
@@ -2,3 +2,4 @@ export { default as AppPicker } from './AppPicker.svelte';
 export { default as Placeholder } from './Placeholder.svelte';
 export { default as LineChart } from './LineChart.svelte';
 export { default as Sidebar } from './Sidebar.svelte';
+export { default as SetupChecklist } from './SetupChecklist.svelte';
