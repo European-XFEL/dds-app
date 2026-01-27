f953ec66e5ccca34d52d5b7b61221adfdb2a658b
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2026-01-27 14:43:48 +0100

    fix(samples): pass bind references to sample fields
    required to avoid issue w/ state management

--- src/routes/experiment/samples/+page.svelte
+++ src/routes/experiment/samples/+page.svelte
@@ -3,6 +3,9 @@
   import { useSimulationState } from '$lib/state.svelte';
 
   let { sample } = useSimulationState();
+
+  let ground = sample.ground;
+  let excited = sample.excited;
 </script>
 
 <div class="flex flex-col gap-6 @sm:flex-wrap">
@@ -14,7 +17,7 @@
     <SolventInfoCard solvent={sample.solvent} />
   </div>
   <div class="grid grid-cols-2 gap-6 @sm:grid-cols-1">
-    <MoleculeCard bind:molecule={sample.ground} title="Ground Molecule" vizOpen={true} />
-    <MoleculeCard bind:molecule={sample.excited} title="Excited Molecule" vizOpen={true} />
+    <MoleculeCard bind:molecule={ground} title="Ground Molecule" vizOpen={true} />
+    <MoleculeCard bind:molecule={excited} title="Excited Molecule" vizOpen={true} />
   </div>
 </div>
