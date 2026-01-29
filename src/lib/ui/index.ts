86bc0dd64231e84d06c954773b4569614c8e5378
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2026-01-29 09:16:04 +0100

    refactor(nav): move nav components to own dir

--- src/lib/ui/index.ts
+++ src/lib/ui/index.ts
@@ -1,5 +1,3 @@
-export { default as AppPicker } from './AppPicker.svelte';
 export { default as Placeholder } from './Placeholder.svelte';
 export { default as LineChart } from './LineChart.svelte';
-export { default as Sidebar } from './Sidebar.svelte';
 export { default as Latex } from './Latex.svelte';
