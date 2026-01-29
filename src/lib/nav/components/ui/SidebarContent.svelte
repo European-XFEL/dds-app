75b41750d9af3fe8c027feb968f8a13a7562f036
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2026-01-29 09:30:32 +0100

    fix(nav): simplify sidebar content, fix typing issues

--- src/lib/nav/components/ui/SidebarContent.svelte
+++ src/lib/nav/components/ui/SidebarContent.svelte
@@ -13,109 +13,123 @@
     Upload,
   } from '@lucide/svelte';
 
-  const data = {
-    user: {
-      name: 'shadcn',
-      email: 'm@example.com',
-      avatar: '/avatars/shadcn.jpg',
+  import type { Pathname } from '$app/types';
+
+  interface NavItem {
+    title: string;
+    url?: Pathname;
+    icon: typeof LayoutDashboard;
+    disabled?: boolean;
+  }
+
+  interface NavSection {
+    title: string;
+    url?: Pathname;
+    items: NavItem[];
+  }
+
+  let data: NavSection[] = [
+    {
+      title: '',
+      items: [
+        {
+          title: 'Dashboard',
+          url: '/dashboard',
+          icon: LayoutDashboard,
+        },
+        {
+          title: 'Flow',
+          // url: '/flow',
+          icon: Flower,
+          disabled: true,
+        },
+        {
+          title: 'Docs',
+          url: '/docs',
+          icon: BookOpen,
+        },
+      ],
     },
-    navMain: [
-      {
-        title: '',
-        url: '#',
-        items: [
-          {
-            title: 'Dashboard',
-            url: '/dashboard',
-            icon: LayoutDashboard,
-          },
-          {
-            title: 'Flow',
-            // url: '/flow',
-            icon: Flower,
-            disabled: true,
-          },
-          {
-            title: 'Docs',
-            url: '/docs',
-            icon: BookOpen,
-          },
-        ],
-      },
-      {
-        title: 'Experiment',
-        url: '#',
-        items: [
-          {
-            title: 'Sample',
-            url: '/experiment/samples',
-            icon: TestTube,
-          },
-          {
-            title: 'Pump/Probe',
-            url: '/experiment/pump-probe',
-            icon: Projector,
-          },
-          {
-            title: 'X-Ray Detector',
-            url: '/experiment/detector',
-            icon: Fullscreen,
-          },
-        ],
-      },
-      {
-        title: 'Results',
-        url: '#',
-        items: [
-          {
-            title: 'Compare',
-            // url: '/results/compare',
-            icon: GitCompare,
-            disabled: true,
-          },
-          {
-            title: 'History',
-            // url: '/results/history',
-            icon: History,
-            disabled: true,
-          },
-        ],
-      },
-      {
-        title: 'Define',
-        url: '#',
-        items: [
-          {
-            title: 'Molecular Files',
-            // url: '/define/upload',
-            icon: Upload,
-            disabled: true,
-          },
-          {
-            title: 'Solvents',
-            // url: '/define/solvent',
-            icon: Beaker,
-            disabled: true,
-          },
-          {
-            title: 'Detectors',
-            // url: '/define/detectors',
-            icon: Microscope,
-            disabled: true,
-          },
-        ],
-      },
-    ],
-  };
+    {
+      title: 'Experiment',
+      items: [
+        {
+          title: 'Sample',
+          url: '/experiment/samples',
+          icon: TestTube,
+        },
+        {
+          title: 'Pump/Probe',
+          url: '/experiment/pump-probe',
+          icon: Projector,
+        },
+        {
+          title: 'X-Ray Detector',
+          url: '/experiment/detector',
+          icon: Fullscreen,
+        },
+      ],
+    },
+    {
+      title: 'Results',
+      items: [
+        {
+          title: 'Compare',
+          // url: '/results/compare',
+          icon: GitCompare,
+          disabled: true,
+        },
+        {
+          title: 'History',
+          // url: '/results/history',
+          icon: History,
+          disabled: true,
+        },
+      ],
+    },
+    {
+      title: 'Define',
+      items: [
+        {
+          title: 'Molecular Files',
+          // url: '/define/upload',
+          icon: Upload,
+          disabled: true,
+        },
+        {
+          title: 'Solvents',
+          // url: '/define/solvent',
+          icon: Beaker,
+          disabled: true,
+        },
+        {
+          title: 'Detectors',
+          // url: '/define/detectors',
+          icon: Microscope,
+          disabled: true,
+        },
+      ],
+    },
+  ];
 </script>
 
 <script lang="ts">
   import { resolve } from '$app/paths';
 
   import * as Sidebar from '$shadcn/ui/sidebar/index.js';
+
+  // Resolve the urls in `data`
+  for (const group of data) {
+    for (const item of group.items) {
+      if (item.url) {
+        // @ts-expect-error
+        item.url = resolve(item.url);
+      }
+    }
+  }
 </script>
 
-{#each data.navMain as group (group.title)}
+{#each data as group (group.title)}
   <Sidebar.Group>
     {#if group.title}
       <Sidebar.GroupLabel>{group.title}</Sidebar.GroupLabel>
@@ -133,7 +147,7 @@
                 {item.title}
               {/snippet}
               {#snippet child({ props })}
-                <a href={item?.url ? resolve(item?.url) : undefined} {...props}>
+                <a href={item?.url} {...props}>
                   <item.icon />
                   <span>{item.title}</span>
                 </a>
