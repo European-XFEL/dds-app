16d2114a002f8b306ff5370fa9c24144756067c9
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2026-02-06 05:28:53 +0100

    feat(molecule/table): add loading state, add animations

--- src/lib/molecule/components/MoleculeTableCard.svelte
+++ src/lib/molecule/components/MoleculeTableCard.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+  import { listMolecules } from '$remote';
   import type { Molecule, Molecules } from '../types';
 
+  import { fade } from 'svelte/transition';
+
   import * as Card from '$shadcn/ui/card';
+  import { Spinner } from '$shadcn/ui/spinner';
 
   import MoleculeDataTable from './MoleculeTable.svelte';
 
@@ -18,17 +22,37 @@
     molecules,
     loading = true,
   }: Props = $props();
+
+  // svelte-ignore state_referenced_locally
+  if (!molecules) {
+    listMolecules().then((data) => {
+      molecules = data;
+      loading = false;
+    });
+  }
 </script>
 
 <Card.Root>
-  <Card.Header class="pb-4">
-    <Card.Title class="text-lg">Molecule Library</Card.Title>
+  <Card.Header>
+    <Card.Title>
+      Molecule Library
+      {#if loading}
+        <div transition:fade class="absolute ml-2 inline-block">
+          <Spinner />
+        </div>
+      {/if}
+    </Card.Title>
     <Card.Description>
       Browse available molecules. Use the Ground and Excited buttons to select
       molecules for analysis.
     </Card.Description>
   </Card.Header>
   <Card.Content>
-    <MoleculeDataTable bind:ground bind:excited {molecules} {loading} />
+    <MoleculeDataTable
+      bind:ground
+      bind:excited
+      molecules={molecules || []}
+      {loading}
+    />
   </Card.Content>
 </Card.Root>
