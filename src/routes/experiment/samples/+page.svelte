4b6ac2f96e3c0943c61768646341a74f40883327
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2026-02-06 06:37:17 +0100

    feat(sample): update page to use new components

--- src/routes/experiment/samples/+page.svelte
+++ src/routes/experiment/samples/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import { MoleculeCard, SolventCard, SolventInfoCard } from '$lib/sample';
+  import { MoleculeCardSelection, MoleculeTableCard } from '$lib/molecule';
+  import { SolventCard, SolventInfoCard } from '$lib/sample';
   import { useSimulationState } from '$lib/state.svelte';
 
   let { sample } = useSimulationState();
@@ -8,24 +9,15 @@
   let excited = sample.excited;
 </script>
 
-<div class="flex flex-col gap-6 @sm:flex-wrap">
-  <div class="grid grid-cols-2 gap-6 @sm:grid-cols-1">
+<div class="mt-8 flex flex-col gap-6 @sm:flex-wrap">
+  <div class="grid grid-cols-3 gap-6 @sm:grid-cols-1">
     <SolventCard
       bind:solvent={sample.solvent}
       bind:concentrationSoluteMolar={sample.concentrationSoluteMolar}
     />
     <SolventInfoCard solvent={sample.solvent} />
+    <MoleculeCardSelection {ground} {excited} />
   </div>
-  <div class="grid grid-cols-2 gap-6 @sm:grid-cols-1">
-    <MoleculeCard
-      bind:molecule={ground}
-      title="Ground Molecule"
-      vizOpen={true}
-    />
-    <MoleculeCard
-      bind:molecule={excited}
-      title="Excited Molecule"
-      vizOpen={true}
-    />
-  </div>
+
+  <MoleculeTableCard bind:ground bind:excited />
 </div>
