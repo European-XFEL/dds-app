8d0acf0ec673eb1747d47041cd6d36754de2d0c4
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2026-02-06 05:49:53 +0100

    refactor(molecule): move around component files

---
+++ src/lib/molecule/components/MoleculeCard.svelte
@@ -0,0 +1,136 @@
+<script lang="ts">
+  import { listMolecules } from '$remote';
+
+  import * as Card from '$shadcn/ui/card/index.js';
+  import * as Field from '$shadcn/ui/field/index.js';
+  import * as Select from '$shadcn/ui/select/index.js';
+  import { Spinner } from '$shadcn/ui/spinner';
+
+  import type { Sample } from '$lib/types';
+
+  import MoleculeViz from './ui/MoleculeViz.svelte';
+
+  type Molecules = Awaited<ReturnType<typeof listMolecules>>;
+
+  type Props = {
+    ground?: Sample['ground'];
+    excited?: Sample['excited'];
+    molecules?: Molecules;
+    vizOpen?: boolean;
+    vizCollapseShow?: boolean;
+    loading?: boolean;
+  };
+
+  let {
+    ground = $bindable(),
+    excited = $bindable(),
+    molecules,
+    loading = true,
+    vizOpen = false,
+    vizCollapseShow = false,
+  }: Props = $props();
+
+  // svelte-ignore state_referenced_locally
+  if (!molecules) {
+    listMolecules().then((data) => {
+      molecules = data;
+      loading = false;
+    });
+  }
+
+  let moleculesNames = $derived(
+    new Set(molecules?.map((m) => m.moleculeName)) ?? [],
+  );
+
+  let moleculeName = $state<string>();
+</script>
+
+<Card.Root>
+  <Card.Header>
+    <Card.Title>
+      Molecule Selection
+      {#if loading}
+        <div class="absolute ml-2 inline-block">
+          <Spinner />
+        </div>
+      {/if}
+    </Card.Title>
+  </Card.Header>
+
+  <Card.Content>
+    <form>
+      <Field.Group>
+        <Field.Set>
+          <Field.Description>...</Field.Description>
+          <Field.Group>
+            <Field.Field>
+              <Field.Label>Molecule</Field.Label>
+              <Select.Root
+                name="molecule"
+                type="single"
+                disabled={loading}
+                bind:value={moleculeName}
+              >
+                <Select.Trigger class="w-full justify-between">
+                  {moleculeName || 'Select molecule'}
+                </Select.Trigger>
+                <Select.Content class="w-(--radix-select-trigger-width)">
+                  {#each moleculesNames as moleculeName (moleculeName)}
+                    <Select.Item value={moleculeName} label={moleculeName}>
+                      {moleculeName}
+                    </Select.Item>
+                  {/each}
+                </Select.Content>
+              </Select.Root>
+            </Field.Field>
+          </Field.Group>
+        </Field.Set>
+
+        <Field.Separator />
+
+        <Field.Set class="flex flex-row gap-4">
+          <Field.Field>
+            <Field.Label>Ground State</Field.Label>
+            <Select.Root type="single">
+              <Select.Trigger>0</Select.Trigger>
+              <Select.Content>
+                <Select.Item value="0">0</Select.Item>
+              </Select.Content>
+            </Select.Root>
+          </Field.Field>
+          <Field.Field>
+            <Field.Label>Excited State</Field.Label>
+            <Select.Root type="single">
+              <Select.Trigger>0</Select.Trigger>
+              <Select.Content>
+                <Select.Item value="0">0</Select.Item>
+              </Select.Content>
+            </Select.Root>
+          </Field.Field>
+        </Field.Set>
+      </Field.Group>
+      <!-- <Field.Field>
+          <Field.Content class="items-stretch gap-2 sm:flex-row">
+          </Field.Content>
+        </Field.Field> -->
+      <!-- {#if vizCollapseShow}
+        <Collapsible.Root bind:open={vizOpen} class="w-full">
+          <Collapsible.Trigger
+            class="mb-2 w-full rounded-md bg-secondary/10 px-3 py-2 text-sm font-medium hover:bg-secondary/20"
+          >
+            {#if vizOpen}
+              Hide Molecule Visualization
+            {:else}
+              Show Molecule Visualization
+            {/if}
+          </Collapsible.Trigger>
+          <Collapsible.Content class="w-full">
+            <MoleculeViz {molecule} />
+          </Collapsible.Content>
+        </Collapsible.Root>
+      {:else if vizOpen}
+        <MoleculeViz {molecule} />
+      {/if} -->
+    </form>
+  </Card.Content>
+</Card.Root>
---
+++ src/lib/molecule/index.ts
@@ -0,0 +1,3 @@
+export { default as MoleculeCard } from './components/MoleculeCard.svelte';
+export { default as MoleculeTableCard } from './components/MoleculeTableCard.svelte';
+export { default as MoleculeCardSelection } from './components/MoleculeCardSelection.svelte';
