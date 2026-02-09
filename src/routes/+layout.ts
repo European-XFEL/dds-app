fdbdf3d5fac689f5127c770494edc221b402bf67
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2026-02-09 11:32:51 +0100

    feat(state): more central state simplifications

--- src/routes/+layout.ts
+++
@@ -1,9 +0,0 @@
-import type { LayoutLoad } from '$types';
-
-import { createSimulationSeed } from '$lib/state.svelte';
-
-export const load: LayoutLoad = async () => {
-  return {
-    simulationSeed: createSimulationSeed(),
-  };
-};
