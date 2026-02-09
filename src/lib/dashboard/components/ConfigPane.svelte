3eab27d46f688b79e95c87062538b3a483766eb1
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2026-02-09 13:36:07 +0100

    refactor(dashboard): split into pane components

---
+++ src/lib/dashboard/components/ConfigPane.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { DetectorSetupCard } from '$lib/detector';
+  import { MoleculeCard } from '$lib/molecule';
+  import { PumpSetupCard } from '$lib/pump';
+  import { SolventCard } from '$lib/sample';
+
+  const { simulation, molecules, solvents, short } = $props();
+</script>
+
+<div class="grid h-72 gap-4">
+  <SolventCard
+    {solvents}
+    bind:concentrationSoluteMolar={simulation.sample.concentrationSoluteMolar}
+    bind:solvent={simulation.sample.solvent}
+    {short}
+  />
+  <MoleculeCard
+    {molecules}
+    bind:ground={simulation.sample.ground}
+    bind:excited={simulation.sample.excited}
+  />
+  <DetectorSetupCard
+    bind:distance={simulation.detector.distance}
+    bind:beamCenter={simulation.detector.beamCenter}
+  />
+  <PumpSetupCard bind:pump={simulation.pump} />
+</div>
---
+++ src/lib/dashboard/components/ResultsPane.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { Badge } from '$shadcn/ui/badge';
+
+  import DSSPlot from './DSSPlot.svelte';
+
+  interface Props {
+    qValues: number[];
+    deltaSi: number[] | null;
+    deltaSSoluteScaled: number[] | null;
+    deltaSSolvent: number[] | null;
+    deltaTemperatureK: number | undefined;
+    depositedEnergyJoule: number | undefined;
+    detectorQRange: [number | null, number | null];
+  }
+
+  const { ...results }: Props = $props();
+</script>
+
+<div class="flex h-lvh flex-col items-center gap-6 overflow-hidden p-4">
+  <h1 class="text-2xl font-bold">Simulation Results</h1>
+
+  <div class="flow-row w-max items-center gap-3">
+    <Badge variant="outline">
+      Delta T (K): {results?.deltaTemperatureK?.toExponential(3) ?? 'N/A'}
+    </Badge>
+
+    <Badge variant="outline">
+      Deposited Energy (J): {results?.depositedEnergyJoule ?? 'N/A'}
+    </Badge>
+  </div>
+  <!-- TODO: Add warning based on the expected temperature range that the dSdT data can apply to? -->
+
+  <div class="w-full rounded-lg bg-secondary/10 p-4">
+    <DSSPlot {...results} />
+  </div>
+</div>
