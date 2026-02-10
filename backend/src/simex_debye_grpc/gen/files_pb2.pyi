17bb55893d177609087c2355442cd89bc0d2c677
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2026-02-10 06:23:28 +0100

    build(backend): regenerate grpc server files

--- backend/src/simex_debye_grpc/gen/files_pb2.pyi
+++ backend/src/simex_debye_grpc/gen/files_pb2.pyi
@@ -12,6 +12,7 @@ class FileTypes(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     FILE_TYPE_UNSPECIFIED: _ClassVar[FileTypes]
     STRUCTURE_FILE: _ClassVar[FileTypes]
     SOLVENT_FILE: _ClassVar[FileTypes]
+
 FILE_TYPE_UNSPECIFIED: FileTypes
 STRUCTURE_FILE: FileTypes
 SOLVENT_FILE: FileTypes
@@ -21,7 +22,7 @@ class Empty(_message.Message):
     def __init__(self) -> None: ...
 
 class File(_message.Message):
-    __slots__ = ()
+    __slots__ = ("type", "id", "filename", "name", "contents")
     TYPE_FIELD_NUMBER: _ClassVar[int]
     ID_FIELD_NUMBER: _ClassVar[int]
     FILENAME_FIELD_NUMBER: _ClassVar[int]
@@ -32,40 +33,56 @@ class File(_message.Message):
     filename: str
     name: str
     contents: bytes
-    def __init__(self, type: _Optional[_Union[FileTypes, str]] = ..., id: _Optional[str] = ..., filename: _Optional[str] = ..., name: _Optional[str] = ..., contents: _Optional[bytes] = ...) -> None: ...
+    def __init__(
+        self,
+        type: _Optional[_Union[FileTypes, str]] = ...,
+        id: _Optional[str] = ...,
+        filename: _Optional[str] = ...,
+        name: _Optional[str] = ...,
+        contents: _Optional[bytes] = ...,
+    ) -> None: ...
 
 class FileMeta(_message.Message):
-    __slots__ = ()
+    __slots__ = ("id", "filename", "name")
     ID_FIELD_NUMBER: _ClassVar[int]
     FILENAME_FIELD_NUMBER: _ClassVar[int]
     NAME_FIELD_NUMBER: _ClassVar[int]
     id: str
     filename: str
     name: str
-    def __init__(self, id: _Optional[str] = ..., filename: _Optional[str] = ..., name: _Optional[str] = ...) -> None: ...
+    def __init__(
+        self,
+        id: _Optional[str] = ...,
+        filename: _Optional[str] = ...,
+        name: _Optional[str] = ...,
+    ) -> None: ...
 
 class ListFilesRequest(_message.Message):
-    __slots__ = ()
+    __slots__ = ("type",)
     TYPE_FIELD_NUMBER: _ClassVar[int]
     type: FileTypes
     def __init__(self, type: _Optional[_Union[FileTypes, str]] = ...) -> None: ...
 
 class ListFilesResponse(_message.Message):
-    __slots__ = ()
+    __slots__ = ("files",)
     FILES_FIELD_NUMBER: _ClassVar[int]
     files: _containers.RepeatedCompositeFieldContainer[FileMeta]
-    def __init__(self, files: _Optional[_Iterable[_Union[FileMeta, _Mapping]]] = ...) -> None: ...
+    def __init__(
+        self, files: _Optional[_Iterable[_Union[FileMeta, _Mapping]]] = ...
+    ) -> None: ...
 
 class GetFileRequest(_message.Message):
-    __slots__ = ()
+    __slots__ = ("type", "id")
     TYPE_FIELD_NUMBER: _ClassVar[int]
     ID_FIELD_NUMBER: _ClassVar[int]
     type: FileTypes
     id: str
-    def __init__(self, type: _Optional[_Union[FileTypes, str]] = ..., id: _Optional[str] = ...) -> None: ...
+    def __init__(
+        self, type: _Optional[_Union[FileTypes, str]] = ..., id: _Optional[str] = ...
+    ) -> None: ...
 
 class CreateFileRequest(_message.Message):
-    __slots__ = ()
+    __slots__ = ("type", "filename", "name", "contents")
     TYPE_FIELD_NUMBER: _ClassVar[int]
     FILENAME_FIELD_NUMBER: _ClassVar[int]
     NAME_FIELD_NUMBER: _ClassVar[int]
@@ -74,18 +91,26 @@ class CreateFileRequest(_message.Message):
     filename: str
     name: str
     contents: bytes
-    def __init__(self, type: _Optional[_Union[FileTypes, str]] = ..., filename: _Optional[str] = ..., name: _Optional[str] = ..., contents: _Optional[bytes] = ...) -> None: ...
+    def __init__(
+        self,
+        type: _Optional[_Union[FileTypes, str]] = ...,
+        filename: _Optional[str] = ...,
+        name: _Optional[str] = ...,
+        contents: _Optional[bytes] = ...,
+    ) -> None: ...
 
 class UpdateFileRequest(_message.Message):
-    __slots__ = ()
+    __slots__ = ("File",)
     FILE_FIELD_NUMBER: _ClassVar[int]
     File: File
     def __init__(self, File: _Optional[_Union[File, _Mapping]] = ...) -> None: ...
 
 class DeleteFileRequest(_message.Message):
-    __slots__ = ()
+    __slots__ = ("type", "id")
     TYPE_FIELD_NUMBER: _ClassVar[int]
     ID_FIELD_NUMBER: _ClassVar[int]
     type: FileTypes
     id: str
-    def __init__(self, type: _Optional[_Union[FileTypes, str]] = ..., id: _Optional[str] = ...) -> None: ...
+    def __init__(
+        self, type: _Optional[_Union[FileTypes, str]] = ..., id: _Optional[str] = ...
+    ) -> None: ...
