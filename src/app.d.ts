0fdac433b49228476aa81b1960bbb17cbd7efb20
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2026-02-10 06:24:22 +0100

    feat(health): add health monitoring

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,14 @@
+import { Capabilities, Health } from '$lib/health.svelte.ts';
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
   namespace App {
     // interface Error {}
-    // interface Locals {}
+    interface Locals {
+      health: Health;
+      capabilities: Capabilities;
+    }
     // interface PageData {}
     // interface PageState {}
     // interface Platform {}
