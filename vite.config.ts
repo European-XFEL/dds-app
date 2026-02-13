d32dda16d548b4c5a176d0ebd7b769fc10384888
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2026-02-13 04:52:56 +0100

    feat(pwa): add pwa config to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import deno from '@deno/vite-plugin';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
@@ -17,6 +18,31 @@ export default defineConfig({
     lucidePreprocess(),
     tailwindcss(),
     sveltekit(),
+    SvelteKitPWA({
+      mode: isProd ? 'production' : 'development',
+      includeAssets: ['favicon.png'],
+      manifest: {
+        name: 'XSS App',
+        short_name: 'XSS',
+        description: 'X-ay solution scattering simulation app',
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'favicon.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: isProd,
+        type: 'module',
+      },
+    }),
     deno(),
     visualizer({
       emitFile: true,
